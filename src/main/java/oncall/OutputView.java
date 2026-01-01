4f8f27a3674a7e83d64622e2f067465894882e68
bee9827 <ehfrhfo9494@naver.com>
2026-01-01 17:38:06 +0900

    fix: 에러 메시지 문구 수정
    [ERROR] + message
    형식으로 출력

--- src/main/java/oncall/OutputView.java
+++ src/main/java/oncall/OutputView.java
@@ -1,8 +1,14 @@
 package oncall;
 
 public class OutputView {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
+    public void printError(String message) {
+        System.out.println(ERROR_PREFIX + message);
+    }
+
     public void printWorker(int month, int day, String dayOfWeek, String nextWorker, boolean isHolidayAndWeekdays) {
-        if(isHolidayAndWeekdays) {
+        if (isHolidayAndWeekdays) {
             dayOfWeek += "(휴일)";
         }
         System.out.printf("%d월 %d일 %s %s%n", month, day, dayOfWeek, nextWorker);
